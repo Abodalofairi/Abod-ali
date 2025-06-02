285e5e5a4cb6f97e98eb7a0049baebb9f7187d3c
Abodalofairi <lbwd86793@gmail.com>
2025-06-02 22:04:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
-    import { defineConfig } from 'vite';
-    import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-    // https://vitejs.dev/config/
-    export default defineConfig({
-      plugins: [react()],
-      build: {
-        outDir: 'dist', // المجلد الذي سيتم فيه إنشاء ملفات البناء
-        rollupOptions: {
-          input: {
-            main: 'index.html', // تحديد index.html كنقطة دخول رئيسية
-          },
-        },
-      },
-    });
-    
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // المجلد الذي سيتم فيه إنشاء ملفات البناء
+    rollupOptions: {
+      input: 'index.html', // تحديد index.html كنقطة دخول رئيسية
+    },
+  },
+});
 
